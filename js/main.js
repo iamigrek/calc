a173234961ca6786bf3945b34bd2e6a84cef4ccb
iamigrek <derevianko.igorek@gmail.com>
2021-07-08 16:25:58 +0300

    Пофиксил баги

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 function insert(num) {
   document.form.textview.value = document.form.textview.value + num;
+  var max = document.form.textview.value;
+  if (max.length > 9) {
+    back();
+  }
 }
 
 function clean() {
   document.form.textview.value = '';
+  var max = document.form.textview.value;
 }
 
 function back() {
   var exp = document.form.textview.value;
   document.form.textview.value = exp.substring(0, exp.length - 1);
+  var max = document.form.textview.value;
 }
 
 function equal() {
