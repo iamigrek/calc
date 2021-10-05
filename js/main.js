ecc8e7480c5de82ed5f994ad3a80a469ac984d21
iamigrek <gerevyanko.igorek@gmail.com>
2021-10-05 18:59:38 +0300

    минифицировал код

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
+const exp = document.form.textview.value;
+
 function insert(num) {
-  document.form.textview.value = document.form.textview.value + num;
-  var max = document.form.textview.value;
-  if (max.length > 9) {
-    back();
-  }
+  let val = document.form.textview.value;
+  document.form.textview.value = val + num;
+  val.length > 8 && back();
+}
+
+function back() {
+  document.form.textview.value = exp.substring(0, exp.length - 1);
 }
 
 function clean() {
   document.form.textview.value = '';
-  var max = document.form.textview.value;
-}
-
-function back() {
-  var exp = document.form.textview.value;
-  document.form.textview.value = exp.substring(0, exp.length - 1);
-  var max = document.form.textview.value;
 }
 
 function equal() {
-  var exp = document.form.textview.value;
-  if (exp) {
-    document.form.textview.value = eval(exp);
-  }
+  exp && (document.form.textview.value = eval(exp));
 }
